1b4297bf8e62d6f93cc3564c3251752dbbb6e73a
ican0422 <ican0422@gmail.com>
2024-10-29 18:13:59 +0900

    feat: 티켓 구매 한 사람만 리뷰를 작성 할 수 있도록 구현

--- reservation-service/src/main/java/com/eatpizzaquickly/reservationservice/review/service/ReviewService.java
+++ reservation-service/src/main/java/com/eatpizzaquickly/reservationservice/review/service/ReviewService.java
@@ -1,7 +1,11 @@
 package com.eatpizzaquickly.reservationservice.review.service;
 
+import com.eatpizzaquickly.reservationservice.common.advice.ApiResponse;
 import com.eatpizzaquickly.reservationservice.common.exception.NotFoundException;
 import com.eatpizzaquickly.reservationservice.common.exception.UnauthorizedException;
+import com.eatpizzaquickly.reservationservice.reservation.entity.Reservation;
+import com.eatpizzaquickly.reservationservice.reservation.entity.ReservationStatus;
+import com.eatpizzaquickly.reservationservice.reservation.repository.ReservationRepository;
 import com.eatpizzaquickly.reservationservice.review.client.concert.ConcertDetailResponse;
 import com.eatpizzaquickly.reservationservice.review.client.concert.ConcertServiceClient;
 import com.eatpizzaquickly.reservationservice.review.client.user.UserResponseDto;
@@ -24,6 +28,7 @@ public class ReviewService {
     private final UserServiceClient userServiceClient;
     private final ConcertServiceClient concertServiceClient;
     private final ReviewRepository reviewRepository;
+    private final ReservationRepository reservationRepository;
 
     /* 댓글 생성 */
     @Transactional
@@ -34,9 +39,14 @@ public class ReviewService {
         userException(userResponseDto);
 
         // 콘서트 아이디 요청
-        ConcertDetailResponse concertDetailResponse = concertServiceClientConcert(concertId);
+        ApiResponse<ConcertDetailResponse> concertDetailResponse = concertServiceClientConcert(concertId);
         // 검증
-        concertException(concertDetailResponse, concertId);
+        concertException(concertDetailResponse);
+
+        // 예매 데이터 가져오기
+        Reservation reservation = reservationOrElseThrow(concertDetailResponse.getData().getConcertId());
+        // 티켓 결제 됐는지 확인
+        reservationException(reservation, ReservationStatus.CONFIRMED);
 
         Review review = Review.builder()
                 .rating(requestDto.getRating())
@@ -44,7 +54,7 @@ public class ReviewService {
                 .nickname(userResponseDto.getNickname())
                 .userEmail(userResponseDto.getEmail())
                 .userId(userId)
-                .concertId(concertId)
+                .concertId(concertDetailResponse.getData().getConcertId())
                 .build();
 
         Review savedReview = reviewRepository.save(review);
@@ -55,12 +65,12 @@ public class ReviewService {
     /* 댓글 조회 */
     public Page<ReviewResponseDto> findReviews(Long concertId, int page, int size) {
         // 콘서트 아이디 요청
-        ConcertDetailResponse concertDetailResponse = concertServiceClientConcert(concertId);
+        ApiResponse<ConcertDetailResponse> concertDetailResponse = concertServiceClientConcert(concertId);
         // 검증
-        concertException(concertDetailResponse, concertId);
+        concertException(concertDetailResponse);
 
         Pageable pageable = PageRequest.of(page - 1, size);
-        Page<Review> reviews = reviewRepository.findAllByConcertId(concertId, pageable);
+        Page<Review> reviews = reviewRepository.findAllByConcertId(concertDetailResponse.getData().getConcertId(), pageable);
         return reviews.map(ReviewResponseDto::from);
     }
 
@@ -73,9 +83,9 @@ public class ReviewService {
         userException(userResponseDto);
 
         // 콘서트 아이디 요청
-        ConcertDetailResponse concertDetailResponse = concertServiceClientConcert(concertId);
+        ApiResponse<ConcertDetailResponse> concertDetailResponse = concertServiceClientConcert(concertId);
         // 검증
-        concertException(concertDetailResponse, concertId);
+        concertException(concertDetailResponse);
 
         // 리뷰 찾기
         Review review = findReview(reviewId);
@@ -98,9 +108,9 @@ public class ReviewService {
         userException(userResponseDto);
 
         // 콘서트 아이디 요청
-        ConcertDetailResponse concertDetailResponse = concertServiceClientConcert(concertId);
+        ApiResponse<ConcertDetailResponse> concertDetailResponse = concertServiceClientConcert(concertId);
         // 검증
-        concertException(concertDetailResponse, concertId);
+        concertException(concertDetailResponse);
 
         // 리뷰 찾기
         Review review = findReview(reviewId);
@@ -120,21 +130,35 @@ public class ReviewService {
     /* 유저 검증 */
     private void userException(UserResponseDto userResponseDto) {
         // 검증
-        if (userResponseDto.getEmail().isEmpty()) {
+        if (userResponseDto.getEmail() == null || !userResponseDto.getEmail().isEmpty()) {
             throw new NotFoundException("해당 유저 정보가 없습니다.");
         }
     }
 
     /* 콘서트 요청 */
-    private ConcertDetailResponse concertServiceClientConcert(Long concertId) {
+    private ApiResponse<ConcertDetailResponse> concertServiceClientConcert(Long concertId) {
         return concertServiceClient.getConcert(concertId);
     }
 
     /* 콘서트 검증 */
-    private void concertException(ConcertDetailResponse concertDetailResponse, Long concertId) {
+    private void concertException(ApiResponse<ConcertDetailResponse> concertDetailResponse) {
         // 검증
-        if (!concertDetailResponse.getConcertId().equals(concertId)) {
+        if (concertDetailResponse == null) {
             throw new NotFoundException("해당 콘서트가 존재하지 않습니다.");
+        }
+    }
+
+    /* 예매 데이터 가져오기 */
+    private Reservation reservationOrElseThrow(Long concertId) {
+        return reservationRepository.findByConcertId(concertId)
+                .orElseThrow(() -> new NotFoundException("해당 예약이 존재하지 않습니다."));
+    }
+
+    /* 티켓 결제 됐는지 확인 */
+    private void reservationException(Reservation reservation, ReservationStatus status) {
+        // 티켓 결제 됐는지 확인
+        if (!reservation.getStatus().equals(status)) {
+            throw new UnauthorizedException("리뷰를 입력할 권한이 없습니다.");
         }
     }
 
