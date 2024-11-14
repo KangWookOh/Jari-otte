a0f78f669b0c565d01b0c0353b5b1894d4fd3c8c
golden-hamster <isack19999@gmail.com>
2024-11-14 16:30:05 +0900

    feat: `concert-service` 에서 Kafka 에러 핸들링 구현 & 보상 트랜잭션 이벤트 소비 구현 (#84)

--- concert-service/src/main/java/com/eatpizzaquickly/concertservice/client/KafkaEventProducer.java
+++ concert-service/src/main/java/com/eatpizzaquickly/concertservice/client/KafkaEventProducer.java
@@ -1,25 +1,28 @@
 package com.eatpizzaquickly.concertservice.client;
 
 import com.eatpizzaquickly.concertservice.dto.SeatReservationEvent;
+import com.eatpizzaquickly.concertservice.service.KafkaFailedMessageService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CompletableFuture;
+
 @RequiredArgsConstructor
+@Slf4j
 @Component
 public class KafkaEventProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
-    private final ObjectMapper objectMapper;
 
-    @Value("${spring.kafka.topic.reservation.created}")
+    @Value("${spring.kafka.topic.reservation-created}")
     private String seatReservationCreatedTopic;
 
-    public void produceSeatReservationEvent(SeatReservationEvent event) throws JsonProcessingException {
-        String message = objectMapper.writeValueAsString(event);
-        kafkaTemplate.send(seatReservationCreatedTopic, message);
+    public void produceSeatReservationEvent(SeatReservationEvent event){
+        kafkaTemplate.send(seatReservationCreatedTopic, event);
     }
-
 }
