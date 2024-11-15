5f1ceff7dc9a610c65eae6cc1819d0e8c3fb8432
ican0422 <ican0422@gmail.com>
2024-11-15 20:03:31 +0900

    refactor: 서비스 쿼리 유틸 클래스로 분리, DTO 변환 유틸 클래스로 분리, 커스텀 예외처리

---
+++ concert-service/src/main/java/com/eatpizzaquickly/concertservice/exception/detail/AutocompleteException.java
@@ -0,0 +1,7 @@
+package com.eatpizzaquickly.concertservice.exception.detail;
+
+public class AutocompleteException extends RuntimeException {
+    public AutocompleteException(String message) {
+        super(message);
+    }
+}
