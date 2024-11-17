1494e1ce7b441ea393654187caa122e7fb63d033
KangWookOh <kanguk456@gmail.com>
2024-11-18 01:43:30 +0900

    feat: fix

--- reservation-service/src/main/java/com/eatpizzaquickly/reservationservice/payment/dto/request/PostPaymentRequest.java
+++ reservation-service/src/main/java/com/eatpizzaquickly/reservationservice/payment/dto/request/PostPaymentRequest.java
@@ -1,8 +1,12 @@
 package com.eatpizzaquickly.reservationservice.payment.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostPaymentRequest {
     private Long reservationId;
     private Long amount;
