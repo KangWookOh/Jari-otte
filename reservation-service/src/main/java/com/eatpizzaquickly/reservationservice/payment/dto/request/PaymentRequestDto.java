30765cbd80afcbd0c9bdbd19b3e94e069e4f4615
Woojin1123 <boom175839@gmail.com>
2024-11-19 11:02:49 +0900

    fix: Json serialize 에러 수정

--- reservation-service/src/main/java/com/eatpizzaquickly/reservationservice/payment/dto/request/PaymentRequestDto.java
+++ reservation-service/src/main/java/com/eatpizzaquickly/reservationservice/payment/dto/request/PaymentRequestDto.java
@@ -1,12 +1,14 @@
-package com.eatpizzaquickly.reservationservice.payment.dto;
+package com.eatpizzaquickly.reservationservice.payment.dto.request;
 
 import com.eatpizzaquickly.reservationservice.payment.entity.PayStatus;
 import com.eatpizzaquickly.reservationservice.payment.entity.SettlementStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PaymentRequestDto {
     private Long id;
     private SettlementStatus settlementStatus;
